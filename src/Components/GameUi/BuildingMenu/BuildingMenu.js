c17db0e25bc27a874e53e8f8abbb9475be04a24a
Sebastian <sebastiangremm97@gmail.com>
2024-03-27 10:37:59 +0100

    Add basic GameUI

---
+++ src/Components/GameUi/BuildingMenu/BuildingMenu.js
@@ -0,0 +1,28 @@
+import React, { useContext } from 'react';
+import { GameContext } from '../../../GameContext';
+
+export const BuildingMenu = () => {
+  const { state, dispatch } = useContext(GameContext);
+
+  const removeWood = () => {
+    dispatch({ type: 'REMOVE_WOOD', payload: 10 });
+  };
+
+  return (
+    <div id="buildingMenu" style={{
+        position: 'absolute',
+        bottom: '0',
+        left: '50%',
+
+    }}>
+        <div 
+            style={{
+                padding: '20px',
+                backgroundColor: 'lightblue',
+                cursor: 'pointer',
+            }}
+            onClick={removeWood} 
+        >Buy Building</div> 
+    </div>
+  );
+};
--- src/Components/GameUi/GameUi.js
+++ src/Components/GameUi/GameUi.js
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react';
 import { GameContext } from '../../GameContext';
 
+import { BuildingMenu } from './BuildingMenu/BuildingMenu';
+
 export const GameUi = () => {
   const { state, dispatch } = useContext(GameContext);
 
@@ -9,14 +11,16 @@ export const GameUi = () => {
   };
 
   return (
-    <div style={{
+    <div id="gameUi" style={{
         position: 'fixed',
         top: '0',
         left: '0',
+        width: '100%',
+        height: '100%',
     }}>
       <p>Woods: {state.wood}</p>
       <button onClick={collectWood}>Collect Wood</button>
-      
+      <BuildingMenu />
     </div>
   );
 };
