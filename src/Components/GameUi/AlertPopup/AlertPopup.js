1f7142afb6206a81235d415f55a02259e0e54f67
Sebastian <sebastiangremm97@gmail.com>
2024-04-27 17:44:50 +0200

    change position of alert text

--- src/Components/GameUi/AlertPopup/AlertPopup.js
+++ src/Components/GameUi/AlertPopup/AlertPopup.js
@@ -1,27 +1,39 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { MyReactState } from '../../../state/ReactContext';
 import './AlertPopup.css'; // Dein vorher definiertes CSS für Alerts
 
 const AlertPopup = () => {
-  const { state: { alert }, dispatch } = useContext(MyReactState);
+    const { state: { alert }, dispatch } = useContext(MyReactState);
+    const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
 
-  useEffect(() => {
-    if (alert.isVisible) {
-      const timer = setTimeout(() => {
-        dispatch({ type: 'hideAlert' });
-      }, 3000);
+    useEffect(() => {
+        const updateCursorPosition = (e) => {
+            setCursorPosition({ x: e.clientX, y: e.clientY });
+        };
+        window.addEventListener('click', updateCursorPosition);
 
-      return () => clearTimeout(timer);
-    }
-  }, [alert.isVisible, dispatch]);
+        return () => {
+            window.removeEventListener('click', updateCursorPosition);
+        };
+    }, []);
 
-  if (!alert.isVisible) return null;
+    useEffect(() => {
+        if (alert.isVisible) {
+            const timer = setTimeout(() => {
+                dispatch({ type: 'hideAlert' });
+            }, 3000);
 
-  return (
-    <div className="alert-box info">
-      {alert.message}
-    </div>
-  );
+            return () => clearTimeout(timer);
+        }
+    }, [alert.isVisible, dispatch]);
+
+    if (!alert.isVisible) return null;
+
+    return (
+        <div className="alert-box info" style={{ position: 'fixed', left: `${cursorPosition.x}px`, top: `${cursorPosition.y}px` }}>
+            {alert.message}
+        </div>
+    );
 };
 
 export default AlertPopup;
